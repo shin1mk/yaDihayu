85229ece0ef23b01f8b7c92f113e16eb03b73022
SHIN MIKHAIL <shin.m.dp@gmail.com>
2024-01-28 15:10:40 +0200

    Initial Commit

---
+++ yaDihayu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yaDihayu
+//
+//  Created by SHIN MIKHAIL on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
